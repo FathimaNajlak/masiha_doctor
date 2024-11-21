899b55205bd6492efcdb1c604684c847f40ccde8
FathimaNajlak <fathnajlahabeeb@gmail.com>
2024-11-21 12:07:29 +0530

    splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:masiha_doctor/widgets/splash/loading_animation.dart';
+import 'package:masiha_doctor/widgets/splash/logo.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      // ignore: use_build_context_synchronously
+      Navigator.pushReplacementNamed(context, '/onboard1');
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Column(
+              children: [LogoWidget(imagepath: 'assets/images/logo.png')],
+            ),
+            const SizedBox(height: 50),
+            LoadingAnimation(controller: _controller),
+          ],
+        ),
+      ),
+    );
+  }
+}
